1e2e534850011f8734a500e541fcf291783bdeb8
Ban-code-art <2124435093@qq.com>
2026-06-15 20:06:02 +0800

    chore: 升级版本到 v0.6.5 (versionCode 16)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.yumark.app"
         minSdk = 26
         targetSdk = 34
-        versionCode = 15
-        versionName = "0.6.4"
+        versionCode = 16
+        versionName = "0.6.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
